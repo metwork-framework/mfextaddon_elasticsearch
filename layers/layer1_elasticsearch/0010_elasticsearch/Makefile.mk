a5af61e985d7d31336d0f65ed9e304b7501bf0e7
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-10-27 14:52:38 +0200

    build: add link to ES binary and environment variable ES_BINARY

--- layers/layer1_elasticsearch/0010_elasticsearch/Makefile.mk
+++ layers/layer1_elasticsearch/0010_elasticsearch/Makefile.mk
@@ -11,9 +11,16 @@ Elasticsearch is a distributed, RESTful search and analytics engine
 WEBSITE=https://www.elastic.co/products/elasticsearch
 LICENSE=Apache
 
+#We provide a link to ES binary in the path ($(PREFIX)/bin/elasticsearch), but it can't be used
+#directly, the binary to use is $(PREFIX)/opt/elasticsearch/bin/elasticsearch
+#The full path to this binary is also provided by environnement variable ES_BINARY
+
 all:: $(PREFIX)/opt/elasticsearch/bin/elasticsearch
 $(PREFIX)/opt/elasticsearch/bin/elasticsearch:
 	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) download uncompress
 	rm -Rf $(PREFIX)/opt/elasticsearch
 	mkdir -p $(PREFIX)/opt
 	cd build && cp -Rf $(NAME)-$(VERSION) $(PREFIX)/opt/elasticsearch
+	mkdir -p $(PREFIX)/bin
+	cd $(PREFIX)/bin && ln -s ../opt/elasticsearch/bin/elasticsearch elasticsearch
+
